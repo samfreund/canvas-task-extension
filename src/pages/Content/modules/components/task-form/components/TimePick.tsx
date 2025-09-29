5905471962dba53818dab0d40429286a2f22fedd
samfreund <samf.236@proton.me>
2025-09-29 11:19:49 -0500

    add 24hr time format support to new task form

--- src/pages/Content/modules/components/task-form/components/TimePick.tsx
+++ src/pages/Content/modules/components/task-form/components/TimePick.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import CourseDropdown from '../../course-dropdown';
 import { DropdownChoice } from '../../course-dropdown/CourseDropdown';
+import useOptions from '../../../hooks/useOptions';
 
 function fmtTime(minutes: number): string {
   const h =
@@ -10,11 +11,18 @@ function fmtTime(minutes: number): string {
   return `${h}:${mm} ${ampm}`;
 }
 
+function fmtTime24(minutes: number): string {
+  const h = Math.floor(minutes / 60);
+  const mm = (minutes % 60 < 10 ? '0' : '') + (minutes % 60);
+  return `${h.toString().padStart(2, '0')}:${mm}`;
+}
+
 type Props = {
   color?: string;
   dark?: boolean;
   selected: string;
   setSelected: (value: string) => void;
+  use24Hour?: boolean;
 };
 
 export default function TimePick({
@@ -22,25 +30,29 @@ export default function TimePick({
   dark,
   selected,
   setSelected,
+  use24Hour = false,
 }: Props): JSX.Element {
   const timeChoices = useMemo(() => {
     const times: DropdownChoice[] = [];
+    const { state: options } = useOptions();
+    const formatFunction = options.clock_24hr ? fmtTime24 : fmtTime;
+    
     for (let i = 0; i < 48; i++) {
       times.push({
         id: i * 30 + '',
-        name: fmtTime(i * 30),
+        name: formatFunction(i * 30),
         color: dark ? 'var(--tfc-dark-mode-text-primary)' : '#2d3b45',
       });
     }
 
     times.push({
       id: '1439',
-      name: fmtTime(1439),
+      name: formatFunction(1439),
       color: dark ? 'var(--tfc-dark-mode-text-primary)' : '#2d3b45',
     });
 
     return times;
-  }, []);
+  }, [use24Hour, dark]);
 
   function chooseTime(id: string) {
     setSelected(id);
